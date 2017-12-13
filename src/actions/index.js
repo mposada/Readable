0630dafb73dc9549c15e61de1e73cfbb463d5e20
mposadar <mposadar@gmail.com>
2017-12-13 12:12:55 -0500

    review fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,21 +9,22 @@ import {
     requestVoteComment
 } from "../utils/ReadableAPI";
 
-export const REQUEST_ALL_POSTS = "REQUEST_ALL_POSTS";
-export const RECEIVE_ALL_POSTS = "RECEIVE_POSTS";
-export const ADD_POST = "ADD_POST";
-export const UPDATE_POST = "UPDATE_POST";
-export const DELETE_POST = "DELETE_POST";
-export const VOTE_POST = "VOTE_POST";
-export const RECEIVE_COMMENTS = "RECEIVE_COMMENTS";
-export const ADD_COMMENT = "ADD_COMMENT";
-export const UPDATE_COMMENT = "UPDATE_COMMENT";
-export const DELETE_COMMENT = "DELETE_COMMENT";
-export const VOTE_COMMENT = "VOTE_COMMENT";
-
-export const FILTER_POSTS_BY_TITLE = "FILTER_POSTS_BY_TITLE";
-export const FILTER_POSTS_SCORE = "FILTER_POSTS_SCORE";
-export const FILTER_POSTS_DATE = "FILTER_POSTS_DATE";
+import {
+    REQUEST_ALL_POSTS,
+    RECEIVE_ALL_POSTS,
+    ADD_POST,
+    UPDATE_POST,
+    DELETE_POST,
+    VOTE_POST,
+    RECEIVE_COMMENTS,
+    ADD_COMMENT,
+    UPDATE_COMMENT,
+    DELETE_COMMENT,
+    VOTE_COMMENT,
+    FILTER_POSTS_BY_TITLE,
+    FILTER_POSTS_SCORE,
+    FILTER_POSTS_DATE
+} from "./actionNames";
 
 // region synchronous-actions
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,80 +14,7 @@ import {
     UPDATE_COMMENT,
     DELETE_COMMENT,
     VOTE_COMMENT
-} from "../actions";
-
-/*const stateShape = {
-    selectedCategory: "react",
-    isFetching: false,
-    filter: "",
-    posts: {
-        react: {
-            isFetching: false,
-            lastUpdated: null,
-            items: [
-                {
-                    id: "8xf0y6ziyjabvozdd253nd",
-                    timestamp: 1467166872634,
-                    title: "Udacity is the best place to learn React",
-                    body: "Everyone says so after all.",
-                    author: "thingtwo",
-                    category: "react",
-                    voteScore: 6,
-                    deleted: false
-                }
-            ]
-        },
-        redux: {
-            isFetching: false,
-            lastUpdated: null,
-            items: [
-                {
-                    id: "6ni6ok3ym7mf1p33lnez",
-                    timestamp: 1468479767190,
-                    title: "Learn Redux in 10 minutes!",
-                    body:
-                        "Just kidding. It takes more than 10 minutes to learn technology.",
-                    author: "thingone",
-                    category: "redux",
-                    voteScore: -5,
-                    deleted: false
-                }
-            ]
-        },
-        udacity: {
-            isFetching: false,
-            items: []
-        }
-    },
-    comments: {
-        "8xf0y6ziyjabvozdd253nd": {
-            isFetching: false,
-            lastUpdated: null,
-            items: [
-                {
-                    id: "894tuq4ut84ut8v4t8wun89g",
-                    parentId: "8xf0y6ziyjabvozdd253nd",
-                    timestamp: 1468166872634,
-                    body: "Hi there! I am a COMMENT.",
-                    author: "thingtwo",
-                    voteScore: 6,
-                    deleted: false,
-                    parentDeleted: false
-                },
-                {
-                    id: "8tu4bsun805n8un48ve89",
-                    parentId: "8xf0y6ziyjabvozdd253nd",
-                    timestamp: 1469479767190,
-                    body: "Comments. Are. Cool.",
-                    author: "thingone",
-                    voteScore: -5,
-                    deleted: false,
-                    parentDeleted: false
-                }
-            ]
-        }
-    }
-};*/
+} from "../actions/actionNames";
 
 function filteredPosts(
     state = {
@@ -191,15 +118,26 @@ function app(
                 }
             };
         case DELETE_COMMENT:
-            return {
-                ...state,
-                comments: {
-                    ...state.comments,
-                    [action.comment.parentId]: state.comments[
-                        action.comment.parentId
-                    ].filter(comment => comment.id != action.comment.id)
-                }
-            };
+            if (state.comments[action.comment.parentId].constructor === Array) {
+                return {
+                    ...state,
+                    comments: {
+                        ...state.comments,
+                        [action.comment.parentId]: state.comments[
+                            action.comment.parentId
+                        ].filter(comment => comment.id !== action.comment.id)
+                    }
+                };
+            } else {
+                return {
+                    ...state,
+                    comments: {
+                        ...state.comments,
+                        [action.comment.parentId]: []
+                    }
+                };
+            }
+
         case ADD_COMMENT:
             return {
                 ...state,
