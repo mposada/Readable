0f5831e24e3870774a85cb68dd6cf07861ef3479
mposadar <mposadar@gmail.com>
2017-12-13 12:18:18 -0500

    suggestions

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Posts from "./Posts";
 import PostDetail from "./PostDetail";
 import "./App.css";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <Router>
-                    <div>
-                        <Route exact={true} path="/" component={Posts} />
-                        <Route exact={true} path="/react" component={Posts} />
-                        <Route exact={true} path="/redux" component={Posts} />
-                        <Route exact={true} path="/udacity" component={Posts} />
-                        <Route path="/:category/:id" component={PostDetail} />
-                    </div>
-                </Router>
-            </div>
-        );
-    }
-}
+const App = props => {
+    return (
+        <div>
+            <Router>
+                <div>
+                    <Route exact={true} path="/" component={Posts} />
+                    <Route exact={true} path="/react" component={Posts} />
+                    <Route exact={true} path="/redux" component={Posts} />
+                    <Route exact={true} path="/udacity" component={Posts} />
+                    <Route path="/:category/:id" component={PostDetail} />
+                </div>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,115 +1,109 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { filterPostsByScore, filterPostsByDate } from "../actions";
 
-class Navigation extends Component {
-    render() {
-        const { selectedCategory, score, dateFilter, dispatch } = this.props;
+const Navigation = props => {
+    const { selectedCategory, score, dateFilter, dispatch } = props;
 
-        return (
-            <aside className="menu">
-                <div className="column-container">
-                    <div className="app-title">My Reads</div>
-                    <nav>
-                        <dl className="category-list">
-                            <dt className="category-title">Categories</dt>
-                            <dd
-                                className={
-                                    "menu-item " +
-                                    (selectedCategory === "All" ? "active" : "")
+    return (
+        <aside className="menu">
+            <div className="column-container">
+                <div className="app-title">My Reads</div>
+                <nav>
+                    <dl className="category-list">
+                        <dt className="category-title">Categories</dt>
+                        <dd
+                            className={
+                                "menu-item " +
+                                (selectedCategory === "All" ? "active" : "")
+                            }
+                        >
+                            <Link to="/">
+                                <i className="fa fa-filter" /> All
+                            </Link>
+                        </dd>
+                        <dd
+                            className={
+                                "menu-item " +
+                                (selectedCategory === "react" ? "active" : "")
+                            }
+                        >
+                            <Link to="/react">
+                                <i className="fa fa-folder-open" /> React
+                            </Link>
+                        </dd>
+                        <dd
+                            className={
+                                "menu-item " +
+                                (selectedCategory === "redux" ? "active" : "")
+                            }
+                        >
+                            <Link to="/redux">
+                                <i className="fa fa-folder-open" /> Redux
+                            </Link>
+                        </dd>
+                        <dd
+                            className={
+                                "menu-item " +
+                                (selectedCategory === "udacity" ? "active" : "")
+                            }
+                        >
+                            <Link to="/udacity">
+                                <i className="fa fa-folder-open" /> Udacity
+                            </Link>
+                        </dd>
+                        <dt className="category-title">Filter</dt>
+                        <dd className="menu-item">
+                            <a
+                                className={score === "HIGH" ? "active" : ""}
+                                onClick={() =>
+                                    dispatch(filterPostsByScore("HIGH"))
                                 }
                             >
-                                <Link to="/">
-                                    <i className="fa fa-filter" /> All
-                                </Link>
-                            </dd>
-                            <dd
-                                className={
-                                    "menu-item " +
-                                    (selectedCategory === "react"
-                                        ? "active"
-                                        : "")
+                                <i className="fa fa-sort-up" /> Major
+                            </a>{" "}
+                            /
+                            <a
+                                className={score === "LOWER" ? "active" : ""}
+                                onClick={() =>
+                                    dispatch(filterPostsByScore("LOWER"))
                                 }
                             >
-                                <Link to="/react">
-                                    <i className="fa fa-folder-open" /> React
-                                </Link>
-                            </dd>
-                            <dd
+                                <i className="fa fa-sort-desc" /> Minor
+                            </a>{" "}
+                            Vote
+                        </dd>
+                        <dd className="menu-item">
+                            <a
                                 className={
-                                    "menu-item " +
-                                    (selectedCategory === "redux"
-                                        ? "active"
-                                        : "")
+                                    dateFilter === "LATEST" ? "active" : ""
+                                }
+                                onClick={() =>
+                                    dispatch(filterPostsByDate("LATEST"))
                                 }
                             >
-                                <Link to="/redux">
-                                    <i className="fa fa-folder-open" /> Redux
-                                </Link>
-                            </dd>
-                            <dd
+                                <i className="fa fa-sort-up" /> Latest
+                            </a>{" "}
+                            /
+                            <a
                                 className={
-                                    "menu-item " +
-                                    (selectedCategory === "udacity"
-                                        ? "active"
-                                        : "")
+                                    dateFilter === "OLDEST" ? "active" : ""
+                                }
+                                onClick={() =>
+                                    dispatch(filterPostsByDate("OLDEST"))
                                 }
                             >
-                                <Link to="/udacity">
-                                    <i className="fa fa-folder-open" /> Udacity
-                                </Link>
-                            </dd>
-                            <dt className="category-title">Filter</dt>
-                            <dd className="menu-item">
-                                <a
-                                    className={score === "HIGH" ? "active" : ""}
-                                    onClick={() =>
-                                        dispatch(filterPostsByScore("HIGH"))}
-                                >
-                                    <i className="fa fa-sort-up" /> Major
-                                </a>{" "}
-                                /
-                                <a
-                                    className={
-                                        score === "LOWER" ? "active" : ""
-                                    }
-                                    onClick={() =>
-                                        dispatch(filterPostsByScore("LOWER"))}
-                                >
-                                    <i className="fa fa-sort-desc" /> Minor
-                                </a>{" "}
-                                Vote
-                            </dd>
-                            <dd className="menu-item">
-                                <a
-                                    className={
-                                        dateFilter === "LATEST" ? "active" : ""
-                                    }
-                                    onClick={() =>
-                                        dispatch(filterPostsByDate("LATEST"))}
-                                >
-                                    <i className="fa fa-sort-up" /> Latest
-                                </a>{" "}
-                                /
-                                <a
-                                    className={
-                                        dateFilter === "OLDEST" ? "active" : ""
-                                    }
-                                    onClick={() =>
-                                        dispatch(filterPostsByDate("OLDEST"))}
-                                >
-                                    <i className="fa fa-sort-desc" /> Oldest
-                                </a>
-                            </dd>
-                        </dl>
-                    </nav>
-                </div>
-            </aside>
-        );
-    }
-}
+                                <i className="fa fa-sort-desc" /> Oldest
+                            </a>
+                        </dd>
+                    </dl>
+                </nav>
+            </div>
+        </aside>
+    );
+};
 
 Navigation.PropTypes = {
     selectedCategory: PropTypes.string.isRequired,
